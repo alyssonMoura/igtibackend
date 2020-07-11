3d357dcb5503810da764a9ff074b0fa217f2f9fe
alyssonMoura <alyssonmoura367@gmail.com>
2020-07-11 01:04:35 -0300

    cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://grades-igti-app.herokuapp.com/",
+    origin: "https://grades-igti-app.herokuapp.com",
   })
 );
 
